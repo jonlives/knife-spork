db44479f22c642a139aa5585583d3cd8de2c038f
Jon Cowie <jcowie@etsy.com>
2012-07-29 16:26:08 +0100

    ui.msg instead of ui.info in spork info

--- lib/chef/knife/spork-info.rb
+++ lib/chef/knife/spork-info.rb
@@ -17,13 +17,13 @@ module KnifeSpork
 
     private
     def info
-			ui.info "Config Hash:"
-			ui.info "#{spork_config.to_hash}"
-			ui.info ""
-			ui.info "Plugins:"
+			ui.msg "Config Hash:"
+			ui.msg "#{spork_config.to_hash}"
+			ui.msg ""
+			ui.msg "Plugins:"
 			KnifeSpork::Plugins.klasses.each do |klass|
 				plugin = klass.new(:config => spork_config)
-        ui.info "#{klass}: #{plugin.enabled? ? 'enabled' : 'disabled'}"
+				ui.msg "#{klass}: #{plugin.enabled? ? 'enabled' : 'disabled'}"
       end
     end
   end
