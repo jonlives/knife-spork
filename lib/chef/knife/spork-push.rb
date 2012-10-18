736b633260e314ad7596f619f15510fe68e8c020
Michael Garrett <garrettux@gmail.com>
2012-10-18 17:30:29 -0400

    added changelog option, methods to open changelog in editor if not set

--- lib/chef/knife/spork-push.rb
+++ lib/chef/knife/spork-push.rb
@@ -9,6 +9,12 @@ module KnifeSpork
 
     banner 'knife spork push COOKBOOK - push changes to github and submit a pull request'
 
+    option :changelog_message,
+      :short => '-m MESSAGE',
+      :long => '--changelog-message MESSAGE',
+      :description => 'Message to add to the changelog for this cookbook version',
+      :default => false
+
     def run
       self.config = Chef::Config.merge!(config)
 
@@ -19,7 +25,14 @@ module KnifeSpork
       end
 
       @cookbook = load_cookbook(name_args.first)
+      changelog_file = File.join(@cookbook.root_dir, "CHANGELOG.md")
 
+      changelog_add_version(changelog_file)
+      if config[:changelog_message] 
+        write_changelog(config[:changelog_message], changelog_file)
+      else
+        edit_changelog(changelog_file)
+      end
       run_plugins(:before_push)
       push
       run_plugins(:after_push)
@@ -27,6 +40,19 @@ module KnifeSpork
 
     private
     def push; end
+
+    def changelog_add_version(file)
+      message = "## #{@cookbook.version}:\n\n"
+      write_changelog(message, file)
+    end
+
+    def edit_changelog(file)
+      system("#{config[:editor]} #{file}")
+    end
+
+    def write_changelog(message, file)
+      File.open(file, 'a') { |file| file.write(message) }
+    end
 
   end
 end
