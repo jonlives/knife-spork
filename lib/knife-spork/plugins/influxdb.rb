da995276f1078c896f6178f9f9c2b10474d0c1ec
Or Elimelech <ore@wix.com>
2014-12-29 16:03:38 +0200

    Added influxdb support for annotation

---
+++ lib/knife-spork/plugins/influxdb.rb
@@ -0,0 +1,28 @@
+require 'knife-spork/plugins/plugin'
+require 'influxdb'
+
+module KnifeSpork
+  module Plugins
+    class Influxdb < Plugin
+      name :influxdb
+      hooks :after_upload
+
+      def perform
+        conn = InfluxDB::Client.new(config.database, host: config.host, port: config.port, username: config.username, password: config.password, use_ssl: config.ssl)
+        environments.each do |environment|
+          begin
+            data = {
+              user: current_user,
+              cookbook: cookbook.name,
+              version: cookbook.version
+            }
+            @conn.write_point(config.series, data)
+          rescue Exception => e
+            ui.error 'Could not write data to influxdb'
+            ui.error e.to_s
+          end
+        end
+      end
+    end
+  end
+end
